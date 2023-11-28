65e86150371ed696314a60e363f103e3fc026bbf
Matthew Kellett <matt.kellett@frogasia.com>
2023-11-28 08:59:46 +0000

    fix(Main): Cleaned up the main script to remove user context

--- src/app/main.py
+++ src/app/main.py
@@ -10,7 +10,6 @@ nltk.download('vader_lexicon')
 sia = SentimentIntensityAnalyzer()
 
 class SentimentRequest(BaseModel):
-  user_uuid: str = Field(..., description="The UUID of the user")
   text: str = Field(..., description="The text to analyse")
 
 app = FastAPI()
@@ -23,16 +22,12 @@ async def index():
 @app.post('/')
 async def analyze_sentiment(request: SentimentRequest):
   try:
-    user_uuid = request.user_uuid.strip()
     text = request.text.strip()
 
     # Perform sentiment analysis
     score = sia.polarity_scores(text)
     sentiment = 'positive' if score['compound'] > 0 else 'negative' if score['compound'] < 0 else 'neutral'
 
-    return {'user_uuid': user_uuid, 'text': text, 'sentiment': sentiment, 'sentiment_score': score}
+    return {'text': text, 'sentiment': sentiment, 'sentiment_score': score}
   except Exception as e:
     raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == '__main__':
-  uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
